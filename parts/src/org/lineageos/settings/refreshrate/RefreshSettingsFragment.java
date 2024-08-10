5c1ebd05581c6a79d82e9237c74f4565f82ad8bb
zaidkhan0997 <kzaid0997@gmail.com>
2024-08-10 16:35:48 +0530

    sm8350-common:parts: Adapt PerApp RefreshRate for lisa
    Anhh Shit we dont have 120hz
    
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- parts/src/org/lineageos/settings/refreshrate/RefreshSettingsFragment.java
+++ parts/src/org/lineageos/settings/refreshrate/RefreshSettingsFragment.java
@@ -197,7 +197,7 @@ public class RefreshSettingsFragment extends PreferenceFragment
             case RefreshUtils.STATE_STANDARD:
                 return R.drawable.ic_refresh_60;
             case RefreshUtils.STATE_EXTREME:
-                return R.drawable.ic_refresh_120;
+                return R.drawable.ic_refresh_90;
             case RefreshUtils.STATE_DEFAULT:
             default:
                 return R.drawable.ic_refresh_default;
